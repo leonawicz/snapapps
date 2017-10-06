13d043a1cd819065e74145032693574885687556
leonawicz <matt.rstat@gmail.com>
2017-10-06 15:38:51 -0600

    Add snapapps.R.

---
+++ R/snapapps.R
@@ -0,0 +1,40 @@
+#' snapapps: Collection of SNAP Shiny apps.
+#'
+#' The \code{snapapps} package contains a collection of open source Shiny apps by Matthew Leonawicz at the Scenarios Network for Alaska and Arctic Planning,
+#' University of Alaska Fairabanks.
+#'
+#' Many of the apps in this package originate \url{https://github.com/ua-snap/shiny-apps}.
+#' \code{snapapps} is a member package in the apps and docs sector of the SNAPverse. Similar sector packages include \code{snapdash},
+#' \code{snapflex} and \code{snapdocs}.
+#'
+#' @docType package
+#' @name snapapps
+NULL
+
+
+#' Run SNAP Shiny app examples
+#'
+#' Launch a Shiny app in your browser.
+#'
+#' Available apps include:
+#'
+#' @param id character, the name of the application. See details.
+#'
+#' There are X appsavailable maps:
+#' \describe{
+#' \item{\code{rv1}}{Distributions of random variables v1}
+#' \item{\code{rv2}}{Distributions of random variables v2}
+#' \item{\code{rv3}}{Distributions of random variables v3}
+#' \item{\code{rv4}}{Distributions of random variables v4}
+#' }
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{snapp("rv4")}
+snapp <- function(id) {
+  appDir <- system.file("shiny-examples", id, package = "snapapps")
+  if (appDir == "")
+    stop("Could not find apps directory. Try re-installing `snapapps`.", call. = FALSE)
+  shiny::runApp(appDir, display.mode = "normal")
+}
